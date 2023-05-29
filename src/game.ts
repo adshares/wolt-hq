ac36c0c9039e2464fcfb301c00336ff0cc9ca322
MykolaZhura <nikoyea14@gmail.com>
2023-05-29 13:29:16 +0200

    add promo-code prompt

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,5 @@
+import * as UI from '@dcl/ui-scene-utils'
+import { ButtonStyles } from '@dcl/ui-scene-utils'
 import { SmartScreen } from './screen'
 import { createWearableVendor } from './wearableVendor'
 
@@ -6,14 +8,13 @@ engine.addEntity(scene)
 
 const build = new Entity()
 build.addComponent(new GLTFShape('models/woltbuild.glb'))
-
 build.setParent(scene)
 
 const screen = new SmartScreen('screen', [
   {
     src: 'images/wolt1.png',
     type: 'image',
-    clickUrl: 'https://google.com'
+    // clickUrl: 'https://google.com'
   },
   {
     src: 'images/wolt2.jpg',
@@ -22,7 +23,7 @@ const screen = new SmartScreen('screen', [
   {
     src: 'images/wolt3.png',
     type: 'image'
-  },
+  }
 ], {
   width: 8.26,
   ratio: '16:9',
@@ -33,8 +34,42 @@ screen.setParent(scene)
 
 const wearableVendor = createWearableVendor(
   'models/woltwearable.glb',
-  new Transform({ position: new Vector3(8, 0.3, 13) }),
+  new Transform({ position: new Vector3(11, 0.3, 11.5) })
   // 'https://google.com'
 )
 wearableVendor.setParent(scene)
+
+const promoCodePopUp = new UI.CustomPrompt(
+  UI.PromptStyles.DARKLARGE,
+  650,
+  550
+)
+promoCodePopUp.addText('Regulamin', 0, 225, Color4.Red(), 50)
+promoCodePopUp.addText('Kod promocyjny', 0, 175, Color4.White(), 25)
+promoCodePopUp.addText('DECENTRALANDWOLT', 0, 125  , Color4.FromHexString('#00C2E8FF'), 25)
+promoCodePopUp.addText(
+  'daje 10 zl znizki na dowolne zamowienie\n ' +
+  'z dostawa na Wolt\n' +
+  '(wazny dla wszystkich uzytkownikow).\n ' +
+  'Kod jest wazny do 30.06.2023,\n' +
+  'a po wpisaniu kodu, srodki sa wazne\n' +
+  'na koncie uzytkownika Wolt przez 7 dni.\n' +
+  'Liczba kodow jest limitowana,\n' +
+  'a limit wpisan kodu wynosi 500 pierwszych osob.',
+  0,
+  -125,
+  Color4.White(),
+  25
+)
+promoCodePopUp.addButton(
+  'OK',
+  0,
+  -225,
+  () => {
+    log('OK')
+    promoCodePopUp.hide()
+  },
+  ButtonStyles.RED
+)
+promoCodePopUp.closeIcon.visible = false
 
